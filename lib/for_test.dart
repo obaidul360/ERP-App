9f048a06d75709555bc47381694421fa07c83570
Md. Obaidul Islam <obdcse360@gmail.com>
2026-04-22 22:52:38 +0600

    singleChildWidget update

--- lib/for_test.dart
+++ lib/for_test.dart
@@ -14,49 +14,53 @@ class _ForTestScreenState extends State<ForTestScreen> {
     return Scaffold(
       appBar: AppBar(title: Text("For test")),
       backgroundColor: Colors.blue,
-      body: Column(
-        children: [
-          Text("Text"),
-          Text("Body"),
+      body: SingleChildScrollView(
+        reverse: true,
+        physics: ScrollPhysics(),
+        child: Column(
+          children: [
+            Text("Text"),
+            Text("Body"),
 
-          SizedBox(height: 140, child: Column(children: [Text("Size Box")])),
-          SizedBox(
-            height: 200,
-            child: Column(children: [Text("2nd Size Box")]),
-          ),
-          SizedBox(height: 20),
-          SizedBox(
-            height: 140,
-            child: Column(
-              children: [
-                Text(
-                  "Width ",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w200,
-                    color: Colors.white,
-                    fontSize: 25,
-                  ),
-                ),
-              ],
+            SizedBox(height: 140, child: Column(children: [Text("Size Box")])),
+            SizedBox(
+              height: 200,
+              child: Column(children: [Text("2nd Size Box")]),
             ),
-          ),
-          SizedBox(
-            height: 200,
-            child: Column(
-              children: [
-                Text(
-                  "Test Size Box",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.yellow,
-                    overflow: TextOverflow.ellipsis,
+            SizedBox(height: 20),
+            SizedBox(
+              height: 140,
+              child: Column(
+                children: [
+                  Text(
+                    "Width ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 200,
+              child: Column(
+                children: [
+                  Text(
+                    "Test Size Box",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.yellow,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
