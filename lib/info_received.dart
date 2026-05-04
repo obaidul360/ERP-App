8062aa06c51d25ba2210e7596ccc73d4dad15c2d
Md. Obaidul Islam <obdcse360@gmail.com>
2026-05-04 23:45:23 +0600

    info received

---
+++ lib/info_received.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfoReceivedScreen extends StatelessWidget {
+  const InfoReceivedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Info Received"),),
+    );
+  }
+}
