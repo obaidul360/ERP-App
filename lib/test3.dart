dd3a3535ad13a03751a245086d1e9601c832c2ce
Md. Obaidul Islam <obdcse360@gmail.com>
2026-05-07 22:31:18 +0600

    add file

---
+++ lib/test3.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class SearchBarExample extends StatefulWidget {
+  const SearchBarExample({super.key});
+
+  @override
+  State<SearchBarExample> createState() => _SearchBarExampleState();
+}
+
+class _SearchBarExampleState extends State<SearchBarExample> {
+  List<String> allItems = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Mango",
+    "Grapes",
+    "Watermelon",
+    "Pizza",
+    "Cheese",
+    "burger",
+    "Tacos/Quesadillas",
+    "Chicken",
+    "Nuggets",
+    "Soup(Tomato, Clam Chowder)Caesar Salad",
+  ];
+
+  List<String> filteredItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredItems = allItems;
+  }
+
+  void searchItem(String value) {
+    setState(() {
+      filteredItems = allItems.where((item) {
+        return item.toLowerCase().contains(value.toLowerCase());
+      }).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Search Bar")),
+
+      body: Column(
+        children: [
+          // Search Bar
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: TextField(
+              onChanged: searchItem,
+
+              decoration: InputDecoration(
+                hintText: "Search here...",
+                prefixIcon: const Icon(Icons.search),
+
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+          ),
+
+          // List
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredItems.length,
+
+              itemBuilder: (context, index) {
+                return Card(child: ListTile(title: Text(filteredItems[index])));
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
